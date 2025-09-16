3d966610e13a03c2ff01f49754b397541c7ce355
stdNullPtr <antonio.liubchev@gmail.com>
2025-09-16 14:11:24 +0300

    feat: initial commit

---
+++ src/main/java/com/stdnullptr/pokeball/Pokeball.java
@@ -0,0 +1,17 @@
+package com.stdnullptr.pokeball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pokeball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
